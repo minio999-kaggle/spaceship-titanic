8fbe55689e4bef281a34412332fd0563e532aed5
minio999 <minkowskidominik69@gmail.com>
2022-05-30 13:50:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     url='',
     license=license,
     include_package_data=True,
-    package_dir={"":"src"},
-    packages=find_packages(where='src'),
+    package_dir={"":"src", "tests":"tests"},
+    packages=find_packages(),
     install_requires=required
 )
