be608fc62e074d83cf9c8e8ca3ea4a560c04ca8d
minio999 <minkowskidominik69@gmail.com>
2022-05-30 14:14:45 +0200

    Update test_pre_proccesing.py

--- tests/test_pre_proccesing.py
+++ tests/test_pre_proccesing.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from preproccesing import encode_tofloat, scaling_features, impute_features, transform_data, get_df
+from ..src.app.preproccessing import encode_tofloat, scaling_features, impute_features, transform_data, get_df
 
 def test_encode_to_float():
     """
